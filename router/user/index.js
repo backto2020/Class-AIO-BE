4c6f5d154c0664c8a357200ae5567aa49127b9e3
backto2020 <backto2020@foxmail.com>
2022-05-25 09:19:47 +0000

    feat(user): delete user api

--- router/user/index.js
+++ router/user/index.js
@@ -42,6 +42,7 @@ const getUser = require('../../utils/user/getUser');
 const verifyUser = require('../../utils/user/verifyUser');
 const newUser = require('../../utils/user/newUser');
 const modifyUser = require('../../utils/user/modifyUser');
+const deleteUser = require('../../utils/user/deleteUser');
 
 user.post('/login', async (ctx, next) => {
   const res = await verifyUser(ctx.request.body.username, ctx.request.body.password);
@@ -103,6 +104,25 @@ user.post('/', async (ctx, next) => {
   }
   const modiUser = ctx.request.body;
   const res = await modifyUser(modiUser);
+  ctx.response.body = res;
+});
+
+user.delete('/:id', async (ctx, next) => {
+  const token = ctx.request.header['x-token'];
+  const user = getUser(token);
+  if (user.constructor === ErrObj) {
+    console.log(user);
+    ctx.response.body = { ...user };
+    return;
+  }
+  if (user.admin === false) {
+    const errObj = new ErrObj(50005, '用户非管理员');
+    ctx.response.body = errObj;
+    return;
+  }
+  const arr1 = ctx.request.url.split('/');
+  const sid = arr1[arr1.length - 1].split('?')[0];
+  const res = await deleteUser(sid);
   ctx.response.body = res;
 });
 
---
+++ utils/user/deleteUser.js
@@ -0,0 +1,37 @@
+const db = require('../db');
+const ErrObj = require('../../utils/ErrObj');
+
+const deleteUser = (sid) => {
+  console.log('Delete a User: ', sid);
+  return new Promise(async (res, rej) => {
+    if (typeof sid === 'undefined') {
+      res(new ErrObj(40115, '未指定学号'));
+    }
+    const exist = await new Promise((res2, _) => {
+      const checkExistSql = `select * from user where sid=${sid}`;
+      db.query(checkExistSql, (err, data) => {
+        if (err) throw err;
+        if (data.length === 0) res2(new ErrObj(40122, '不存在该学号的记录'));
+        res2(true);
+      });
+    });
+    if (exist !== true) {
+      res(exist);
+      return;
+    }
+    const data = await new Promise((res2, _) => {
+      let delSql = `delete from user where sid=${sid}`;
+      console.log(delSql);
+      db.query(delSql, (err, data) => {
+        if (err) throw err;
+        res2({
+          code: 20000,
+          message: '删除成功'
+        });
+      });
+    });
+    res(data);
+  });
+};
+
+module.exports = deleteUser;
