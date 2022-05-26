0bd9325ca7b0142bde78c859a58fd6f6239da4aa
backto2020 <backto2020@foxmail.com>
2022-05-26 13:57:57 +0000

    feat(noti): newNoti API

--- src/router/noti/index.js
+++ src/router/noti/index.js
@@ -1,7 +1,9 @@
 const noti = require('koa-router')();
 const ErrObj = require('../../utils/ErrObj');
 const getUser = require('../../utils/user/getUser');
+const checkAdmin = require('../../utils/user/checkAdmin');
 const getNotiList = require('../../utils/noti/getNotiList');
+const newNoti = require('../../utils/noti/newNoti');
 
 noti.get('/', async (ctx, next) => {
   const user = getUser(ctx.request.header['x-token']);
@@ -10,6 +12,17 @@ noti.get('/', async (ctx, next) => {
     return;
   }
   const res = await getNotiList();
+  ctx.response.body = res;
+});
+
+noti.put('/', async (ctx, next) => {
+  const admin = await checkAdmin(ctx.request.header['x-token']);
+  if (admin.constructor === ErrObj) {
+    ctx.response.body = admin;
+    return;
+  }
+  const newNotiObj = ctx.request.body;
+  const res = await newNoti(newNotiObj);
   ctx.response.body = res;
 });
 
---
+++ src/utils/noti/newNoti.js
@@ -0,0 +1,36 @@
+const db = require('../db');
+const ErrObj = require('../ErrObj');
+const ResObj = require('../ResObj');
+
+const newNoti = (notiObj) => {
+  console.log('New a User: ', notiObj);
+  return new Promise(async (res, rej) => {
+    const { title, time, content, type } = notiObj;
+    if (
+      typeof title === 'undefined' ||
+      typeof time === 'undefined' ||
+      typeof content === 'undefined' ||
+      typeof type === 'undefined'
+    ) {
+      res(new ErrObj(40215, '不满足字段非空的要求'));
+      return;
+    }
+    if (time.constructor !== String) {
+      res(new ErrObj(40216, 'time字段不满足ISOString格式'));
+      return;
+    }
+    const time2 = time.slice(0, 19).replace('T', ' ');
+    const insertSql = `
+      insert into noti
+      (title, time, content, type)
+      values
+      ('${title}', '${time2}', '${content}', '${type}')
+    `;
+    db.query(insertSql, (err, data) => {
+      if (err) throw err;
+      res(new ResObj('创建成功'));
+    });
+  });
+};
+
+module.exports = newNoti;
