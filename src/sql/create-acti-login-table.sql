7edb85a1ea76e061ce0855d2baa7b9ffb9d692bb
backto2020 <backto2020@foxmail.com>
2022-05-27 13:24:15 +0000

    docs(sql): create acti_login table

---
+++ src/sql/create-acti-login-table.sql
@@ -0,0 +1,24 @@
+create table if not exists `acti_login` (
+  `id` int unsigned auto_increment,
+  `sid` int unsigned not null,
+  `aid` int unsigned not null,
+  `time` timestamp not null,
+  primary key ( `id` ),
+  foreign key (`sid`) references user(`sid`),
+  foreign key (`aid`) references acti(`id`)
+);
+
+-- describe acti_login;
+-- +-------+--------------+------+-----+---------+----------------+
+-- | Field | Type         | Null | Key | Default | Extra          |
+-- +-------+--------------+------+-----+---------+----------------+
+-- | id    | int unsigned | NO   | PRI | NULL    | auto_increment |
+-- | sid   | int unsigned | NO   | MUL | NULL    |                |
+-- | aid   | int unsigned | NO   | MUL | NULL    |                |
+-- | time  | timestamp    | NO   |     | NULL    |                |
+-- +-------+--------------+------+-----+---------+----------------+
+
+-- insert into acti_login
+-- (sid, aid, time)
+-- values
+-- (19101010, 5, '2022-05-27 13:00:00');
